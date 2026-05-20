57bd082c29d50717c2a1031f2dd3e5e962c5781d
Lorenzo Facchini <lorenzofacchini06@gmail.com>
2026-05-20 11:55:12 +0200

    fix input.hpp

--- input.hpp
+++ input.hpp
@@ -3,9 +3,11 @@
 
 #include <string>
 #include <sstream>
+#include <SFML/System/Vector2.hpp>
+#include <vector>
 
 namespace nc {
-void inputFromFileConfig(std::string const& fileNameConfig) {
+void inputFromFileConfig(std::string const& fileNameConfig, int& steps, float& dt, int& n_particle, std::vector<sf::Vector2f>& r, std::vector<sf::Vector2f>& v) {
   std::ifstream f_config{fileNameConfig};
   std::string line{};
 
@@ -21,6 +23,15 @@ void inputFromFileConfig(std::string const& fileNameConfig) {
       ss >> dt;
     } else if (key == "n_particle") {
       ss >> n_particle;
+    } else if(key == "r1" || key == "r2" || key == "r3") {
+        float x, y;
+        ss >> x >> y;
+        r.push_back(sf::Vector2f{x, y});
+    }
+    else if(key == "v1" || key == "v2" || key == "v3") {
+        float x, y;
+        ss >> x >> y;
+        v.push_back(sf::Vector2f{x, y});
     }
   }
 }
--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 
 
 #include "input.hpp"
+#include "body.hpp"
+#include <vector>
+#include <SFML/System/Vector2.hpp>
 
 int main() {
+    int steps{};
+    float dt{};
+    int n_particle{};
+    std::vector<nc::Body> container{};
+    std::vector<sf::Vector2f> r{};
+    std::vector<sf::Vector2f> v{};
+    std::string fileName{"file.config"};
+
+    nc::inputFromFileConfig(fileName, steps, dt, n_particle, r, v);
+
+    nc::generateBodies(container, r, v);
  
 }
