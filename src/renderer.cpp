f220e35c021048901c5e21f8aebd7712ee39b7cf
Lorenzo Facchini <lorenzofacchini06@gmail.com>
2026-05-29 13:06:16 +0200

    fix position of CM

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,8 +9,8 @@ void Renderer::draw(sf::RenderWindow& w, std::vector<Body> const& v_bodies) {
   for (auto const& b : v_bodies) {
     sf::CircleShape shape{8.f};
     sf::Vector2f body_pos{
-        b.pos().x * scale, 
-        b.pos().y * scale
+        400.f + b.pos().x * scale, 
+        300.f - b.pos().y * scale
     };
     shape.setPosition(body_pos);
     w.draw(shape);
